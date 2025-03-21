220279f0e8672aa929f646827175314a72f24f58
Prince Mishra <mrprimi91@gmail.com>
2025-03-21 11:03:41 +0530

    ind.js new file

---
+++ ind.js
@@ -0,0 +1,36 @@
+// Scroll to Top Button
+const scrollToTopBtn = document.getElementById("scrollToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+
+// hamburger event =================
+
+let hb = document.querySelector(".hb")
+let nav = document.querySelector("nav")
+let flag = true
+hb.addEventListener('click', () => {
+    if (flag) {
+        nav.style.left = "0"
+        document.body.style.overflow = "hidden"; 
+        flag = false
+    } else {
+        nav.style.left = "-769px"
+        document.body.style.overflow = "auto";
+        flag = true
+    }
+})
